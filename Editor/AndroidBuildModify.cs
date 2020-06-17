1898df86b0ec8e8decfef0eac91da959333f7942
Silas <hdss3609@gmail.com>
2020-06-17 18:08:01 +0800

    Commit Runtime/Editor Script

---
+++ Editor/AndroidBuildModify.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Capstones.UnityEngineEx;
+
+namespace Capstones.UnityEditorEx
+{ 
+    public static class AndroidBuildModify
+    {
+        private class AndroidPreBuild : UnityEditor.Build.IPreprocessBuild, UnityEditor.Build.IPostprocessBuild
+        {
+            public int callbackOrder { get { return 0; } }
+
+            public void OnPreprocessBuild(BuildTarget target, string path)
+            {
+                if (target == BuildTarget.Android)
+                {
+                    string amani = "EditorOutput/Intermediate/AndroidManifest.xml";
+                    //if (!PlatDependant.IsFileExist(amani))
+                    {
+                        var omani = EditorApplication.applicationContentsPath + @"\PlaybackEngines\AndroidPlayer\Apk\AndroidManifest.xml";
+                        if (!PlatDependant.IsFileExist(omani))
+                        {
+                            Debug.LogError("Cannot find original AndroidManifest.xml");
+                            return;
+                        }
+
+                        PlatDependant.CopyFile(omani, amani);
+                    }
+
+                    try
+                    {
+                        var doc = System.Xml.Linq.XDocument.Load(amani);
+                        var appnode = doc.Element("manifest").Element("application").Element("activity");
+                        appnode.Attribute(System.Xml.Linq.XName.Get("name", "http://schemas.android.com/apk/res/android")).SetValue("cn.capstones.anative.android.capsactivity.CapsActivity");
+                        doc.Save(amani);
+                        PlatDependant.CopyFile(amani, "Assets/Plugins/Android/AndroidManifest.xml");
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
+                }
+            }
+
+            public void OnPostprocessBuild(BuildTarget target, string path)
+            {
+                PlatDependant.DeleteFile("EditorOutput/Intermediate/AndroidManifest.xml");
+            }
+        }
+    }
+}
---
+++ Runtime/SDKEntry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Capstones.UnityEngineEx.Native
+{
+    public static class SDKEntry
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        private static void Init()
+        {
+            using (AndroidJavaClass jc = new AndroidJavaClass("cn.capstones.anative.android.sdkplugin.SDKPlugin"))
+            {
+                jc.CallStatic("Init");
+            }
+        }
+
+#else
+        private static void Init() { }
+#endif
+
+        [UnityEngine.RuntimeInitializeOnLoadMethod]
+        private static void OnUnityStart()
+        {
+            ResManager.AddInitItem(new ResManager.ActionInitItem(ResManager.LifetimeOrders.CrossEvent + 7, Init, null));
+        }
+    }
+}
