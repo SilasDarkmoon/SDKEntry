3efa323941f19b3cdb9c5d995ef7274909ea5fc1
Silas <hdss3609@gmail.com>
2021-12-02 12:09:44 +0800

    使用IPostGenerateGradleAndroidProject来修改 AndroidManifest.xml

--- Editor/AndroidBuildModify.cs
+++ Editor/AndroidBuildModify.cs
@@ -10,6 +10,111 @@ namespace Capstones.UnityEditorEx
 { 
     public static class AndroidBuildModify
     {
+        public static string FindAndroidProjectRoot(string path)
+        {
+            var seaching = path;
+            while (!string.IsNullOrEmpty(seaching))
+            {
+                bool found = false;
+                var file = System.IO.Path.Combine(seaching, "build.gradle");
+                if (System.IO.File.Exists(file))
+                {
+                    foreach (var line in System.IO.File.ReadLines(file))
+                    {
+                        if (line.TrimStart().StartsWith("classpath 'com.android.tools.build:gradle:"))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+                seaching = System.IO.Path.GetDirectoryName(seaching);
+            }
+            return string.IsNullOrEmpty(seaching) ? path : seaching;
+        }
+        public static string FindAndroidProjectAppFolder(string root)
+        {
+            var targetname = System.IO.Path.Combine(root, "launcher");
+            if (System.IO.File.Exists(targetname + "/build.gradle"))
+            {
+                return targetname;
+            }
+            var subs = System.IO.Directory.GetDirectories(root);
+            foreach (var sub in subs)
+            {
+                var file = System.IO.Path.Combine(sub, "build.gradle");
+                if (System.IO.File.Exists(file))
+                {
+                    foreach (var line in System.IO.File.ReadLines(file))
+                    {
+                        if (line.TrimStart().StartsWith("applicationId '"))
+                        {
+                            return sub;
+                        }
+                    }
+                }
+            }
+            return root;
+        }
+        public static string FindAndroidProjectUnityLibFolder(string root)
+        {
+            var targetname = System.IO.Path.Combine(root, "unityLibrary");
+            if (System.IO.File.Exists(targetname + "/build.gradle"))
+            {
+                return targetname;
+            }
+            var subs = System.IO.Directory.GetDirectories(root);
+            foreach (var sub in subs)
+            {
+                var file = System.IO.Path.Combine(sub, "build.gradle");
+                if (System.IO.File.Exists(file))
+                {
+                    bool islib = false;
+                    foreach (var line in System.IO.File.ReadLines(file))
+                    {
+                        if (!islib)
+                        {
+                            var linet = line.Trim();
+                            if (linet.StartsWith("apply plugin") && linet.EndsWith("'com.android.library'"))
+                            {
+                                islib = true;
+                            }
+                        }
+                        else
+                        {
+                            if (line.Contains("unity", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                return sub;
+                            }
+                        }
+                    }
+                }
+            }
+            return root;
+        }
+
+
+#if UNITY_2018_2_OR_NEWER
+        private class AndroidPostBuild : UnityEditor.Android.IPostGenerateGradleAndroidProject
+        {
+            public int callbackOrder { get { return 0; } }
+
+            public void OnPostGenerateGradleAndroidProject(string path)
+            {
+                var proot = FindAndroidProjectRoot(path);
+                var ulibroot = FindAndroidProjectUnityLibFolder(proot);
+                string amani = System.IO.Path.Combine(ulibroot, "src/main/AndroidManifest.xml");
+                var doc = System.Xml.Linq.XDocument.Load(amani);
+                var appnode = doc.Element("manifest").Element("application").Element("activity");
+                appnode.Attribute(System.Xml.Linq.XName.Get("name", "http://schemas.android.com/apk/res/android")).SetValue("cn.capstones.anative.android.capsactivity.CapsActivity");
+                doc.Save(amani);
+            }
+        }
+#else
         private class AndroidPreBuild : UnityEditor.Build.IPreprocessBuild, UnityEditor.Build.IPostprocessBuild
         {
             public int callbackOrder { get { return 0; } }
@@ -55,5 +160,6 @@ namespace Capstones.UnityEditorEx
                 PlatDependant.DeleteFile("EditorOutput/Intermediate/AndroidManifest.xml");
             }
         }
+#endif
     }
 }
