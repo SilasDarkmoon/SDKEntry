964a46e96d9589944960223df32bfae3d1c71543
Silas <hdss3609@gmail.com>
2022-08-04 16:39:55 +0800

    SDKPluginWebView

---
+++ ~PluginSrc~/CapsActivityProject/sdkpluginwebview/src/main/java/cn/capstones/anative/sdkpluginwebview/SDKPluginWebView.java
@@ -0,0 +1,14 @@
+package cn.capstones.anative.sdkpluginwebview;
+
+import android.util.Log;
+
+import cn.capstones.anative.android.sdkplugin.SDKPlugin;
+
+public class SDKPluginWebView extends SDKPlugin
+{
+    @Override
+    public void InitEvents()
+    {
+        Log.d("SDKPluginWebView", "SDKPluginWebView.InitEvents");
+    }
+}
